b6adcbc57d82f1248a956ea3c0d26fdff261ae22
unknown <m_fattih@hotmail.com>
2019-10-08 20:25:56 -0700

    JS-III

--- assignments/this.js
+++ assignments/this.js
@@ -10,17 +10,55 @@
 */
 
 // Principle 1
-
+  
 // code example for Window Binding
 
+function car (sound){
+    console.log(this);
+    return sound;
+}
+
+car('Vrooooom!');
+var carSound='Vrooooom!';
+
+//---------------------------------------------------------------
 // Principle 2
 
 // code example for Implicit Binding
 
+let myCusine = {
+    name:'Kebab',
+    ingredients: 'Lamb meat',
+    nameOfthedish: function(){
+       // console.log('${this.name} made from ${this.ingredients}');
+       console.log(this.name +" made from "+this.ingredients);
+    }
+}
+myCusine.nameOfthedish();
+//---------------------------------------------------------------
 // Principle 3
 
 // code example for New Binding
 
+function GermanCar(brand){
+    this.germanBrand = brand;
+}
+
+let myGermanCar = new GermanCar('Mercedes is made in Germany');
+console.log(myGermanCar.germanBrand);
+//--------------------------------------------------------------------
+
 // Principle 4
 
 // code example for Explicit Binding
+
+function airline () {
+    console.log(this.name);
+}
+
+let myAirline = {
+    name: 'Turkish Airlines',
+    airplane: 'Boeing-777' 
+}
+
+airline.call(myAirline);
